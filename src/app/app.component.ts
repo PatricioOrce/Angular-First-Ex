be4fdba09c2ad3e2eb5882c377f8c047f0a9a0df
Patricio <patodotaop@hotmail.com>
2023-03-30 22:22:52 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = '';
+
+  edadUno: number = 0;
+  edadDos: number = 0;
+  resultadoProm: number = 0;
+  resultadoSum: number = 0;
+  imagen = '../assets/Pato.png';
+  calcular(){
+    this.resultadoProm = (this.edadUno+Number(this.edadDos))/2;
+    this.resultadoSum = (this.edadUno+Number(this.edadDos));
+  }
+  Limpiar(){
+    this.resultadoProm = 0;
+    this.resultadoSum = 0;
+    
+  }
 }
